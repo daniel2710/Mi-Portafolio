c8ff3d9162526fcbfc219b46d69b1c73fd2825fe
Daniel De Ávila <67779656+daniel2710@users.noreply.github.com>
2022-07-15 11:23:06 -0500

    pixabay add

--- src/pages/Proyectos.jsx
+++ src/pages/Proyectos.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import SystemImg from '../assets/img/system-easy.png';
 import ErrorImg from '../assets/img/error-404-img.png';
 import FoodsImg from '../assets/img/magnific-foods-img.png';
+import PixabayImg from "../assets/img/pixabay.png";
 import '../styles/proyectos.scss';
 
 
@@ -12,7 +13,8 @@ const Proyectos = () => {
       <div className='col-md-6 d-flex justify-content-around mt-4'>
         
         <div className="card card-proyect mb-4">
-          <img width={50} height={150} src={SystemImg} className="card-img-top" alt="..." />
+
+          <img width={50} height={150} src={SystemImg} className="card-img-top" alt="Proyecto1" />
           <div className="card-body">
             <h5 className="card-title">System Inventory</h5>
             <span className="badge text-bg-primary m-1">React Js</span>
@@ -20,7 +22,7 @@ const Proyectos = () => {
             <span className="badge text-bg-dark m-1">Express</span>
             <span className="badge text-bg-success m-1">Mongo Db</span>
             <p className="card-text text-secondary">Se trata de un crud donde podemos agregar equipos y agregar mantenimientos a dicho equipo.</p>
-            <p>Credenciales: admin@miadmin.com / 123456</p>
+            <p>acceso: admin@miadmin.com/123456</p>
             <div className='d-flex justify-content-around'>
               <a href="https://client-system-easy.onrender.com/" rel="noreferrer noopener" target='_blank' className="btn btn-proyect">Demo</a>
               <a href="https://github.com/daniel2710/system-easy-crud" rel="noreferrer noopener" target='_blank' className="btn btn-proyect">Code</a>
@@ -29,7 +31,26 @@ const Proyectos = () => {
         </div>
 
         <div className="card card-proyect mb-4">
-          <img width={50} height={150} src={FoodsImg} className="card-img-top" alt="..." />
+          <img width={50} height={150} src={PixabayImg} className="card-img-top" alt="Proyecto4" />
+          <div className="card-body">
+            <h5 className="card-title">BuscaPixabay - Buscador de imagenes</h5>
+            <span className="badge text-bg-primary m-1">React Js</span>
+            <span className="badge text-bg-danger m-1">API</span>
+            <p className="card-text text-secondary">Banco de imagenes consumiendo la api de pixabay.</p>
+            <div className='d-flex justify-content-around'>
+              <a href="https://buscapixabay.onrender.com/" rel="noreferrer noopener" target='_blank' className="btn btn-proyect">Demo</a>
+              <a href="https://github.com/daniel2710/BuscaPixabay" rel="noreferrer noopener" target='_blank' className="btn btn-proyect">Code</a>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+
+      <div className='col-md-6 d-flex justify-content-around mt-4'>
+
+        <div className="card card-proyect mb-4">
+          <img width={50} height={150} src={FoodsImg} className="card-img-top" alt="Proyecto2" />
           <div className="card-body">
             <h5 className="card-title">Landing Page - Magnific Foods</h5>
             <span className="badge text-bg-primary m-1">React Js</span>
@@ -40,13 +61,9 @@ const Proyectos = () => {
             </div>
           </div>
         </div>
-
-      </div>
-
-
-      <div className='col-md-6 d-flex justify-content-around mt-4'>
-      <div className="card card-proyect mb-4">
-          <img width={50} height={150} src={ErrorImg} className="card-img-top" alt="..." />
+      
+        <div className="card card-proyect mb-4">
+          <img width={50} height={150} src={ErrorImg} className="card-img-top" alt="Proyecto3" />
           <div className="card-body">
             <h5 className="card-title">Error 404 page</h5>
             <span className="badge text-bg-primary m-1">React Js</span>
@@ -58,18 +75,8 @@ const Proyectos = () => {
           </div>
         </div>
 
-        <div className="card card-proyect mb-4">
-          <img width={50} height={150} src="https://assets-global.website-files.com/6009ec8cda7f305645c9d91b/602f2109a787c146dcbe2b66_601b1c1f7567a7399353fe47_traackr.jpeg" className="card-img-top" alt="..." />
-          <div className="card-body">
-            <h5 className="card-title">en proceso...</h5>
-            <p className="card-text text-secondary">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <div className='d-flex justify-content-around'>
-              <a href="/" className="btn btn-proyect">Demo</a>
-              <a href="/" className="btn btn-proyect">Code</a>
-            </div>
-          </div>
-        </div>
       </div>
+    
     </div>
   )
 }
